39a95b6cb418be618eaa102624b3e157bb9b93ba
Chirag Madan <chiragmadan4@gmail.com>
2020-06-13 00:34:35 +0530

    add scroll effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 window.onscroll = function() {
     var x = window.scrollY;
     var nav = document.querySelector(".nav_bar");
@@ -11,4 +12,20 @@ window.onscroll = function() {
     } else {
         nav.classList.remove("scrolled");
     }
+}
+
+document.querySelector("#start_btn").onclick = function() {
+    document.querySelector("#about").scrollIntoView({behavior: "smooth"});
+}
+
+document.querySelector("#about_btn").onclick = function() {
+    document.querySelector("#about").scrollIntoView({behavior: "smooth"});
+}
+
+document.querySelector("#work_btn").onclick = function() {
+    document.querySelector("#work").scrollIntoView({behavior: "smooth"});
+}
+
+document.querySelector("#contact_btn").onclick = function() {
+    document.querySelector("#contact").scrollIntoView({behavior: "smooth"});
 }
